5a582f5993aed6d88d7c3c57f1e9d9b526054b4b
Bruno Léon <bruno.leon@suse.com>
2019-10-11 15:32:14 +0200

    add skuba deployment

---
+++ test/02skuba_test.go
@@ -0,0 +1,85 @@
+package test
+
+import (
+	"testing"
+  "fmt"
+
+	"github.com/gruntwork-io/terratest/modules/shell"
+  "github.com/stretchr/testify/assert"
+  test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+)
+
+func Test(t *testing.T) {
+	t.Parallel()
+
+	// Deploy skuba
+	test_structure.RunTestStage(t, "skubaInit", func() {
+		skubaInit(t)
+	})
+	test_structure.RunTestStage(t, "skubaBootstrap", func() {
+		skubaBootstrap(t)
+	})
+	test_structure.RunTestStage(t, "skubaJoin1", func() {
+		skubaJoin1(t)
+	})
+	test_structure.RunTestStage(t, "skubaJoin2", func() {
+		skubaJoin2(t)
+	})
+}
+
+func skubaInit(t *testing.T) {
+  cmdArgs := []string{}
+  cmdArgs = append(cmdArgs, "cluster", "init", "--control-plane", "testing-lb.caasp.local", "company-cluster")
+
+  cmd := shell.Command{
+    Command: "skuba",
+    Args:    cmdArgs,
+  }
+  out, err :=  shell.RunCommandAndGetOutputE(t, cmd)
+  fmt.Println(out)
+  fmt.Println(err)
+
+  assert.DirExists(t, "company-cluster")
+}
+
+func skubaBootstrap(t *testing.T) {
+  cmdArgs := []string{}
+  cmdArgs = append(cmdArgs, "node", "bootstrap", "--user", "sles", "--sudo", "--target", "10.17.2.0", "my-master")
+
+  cmd := shell.Command{
+    Command: "skuba",
+    Args:    cmdArgs,
+    WorkingDir: "company-cluster",
+  }
+  out, err :=  shell.RunCommandAndGetOutputE(t, cmd)
+  fmt.Println(out)
+  fmt.Println(err)
+}
+
+func skubaJoin1(t *testing.T) {
+  cmdArgs := []string{}
+  cmdArgs = append(cmdArgs, "node", "join", "--role", "worker", "--user", "sles", "--sudo", "--target", "10.17.3.0", "worker0")
+
+  cmd := shell.Command{
+    Command: "skuba",
+    Args:    cmdArgs,
+    WorkingDir: "company-cluster",
+  }
+  out, err :=  shell.RunCommandAndGetOutputE(t, cmd)
+  fmt.Println(out)
+  fmt.Println(err)
+}
+
+func skubaJoin2(t *testing.T) {
+  cmdArgs := []string{}
+  cmdArgs = append(cmdArgs, "node", "join", "--role", "worker", "--user", "sles", "--sudo", "--target", "10.17.3.1", "worker1")
+
+  cmd := shell.Command{
+    Command: "skuba",
+    Args:    cmdArgs,
+    WorkingDir: "company-cluster",
+  }
+  out, err :=  shell.RunCommandAndGetOutputE(t, cmd)
+  fmt.Println(out)
+  fmt.Println(err)
+}
