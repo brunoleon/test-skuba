e7a73f56b3ee45f03eb53a772bf84b7180d8cfaf
Bruno Léon <bruno.leon@suse.com>
2019-10-14 13:29:34 +0200

    split skuba parts

--- test/01terraform_test.go
+++ test/01terraform_test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"testing"
+  "fmt"
+  "os"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -39,14 +41,93 @@ func TestTerraformBasicExample(t *testing.T) {
 
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
+//  skuba(t)
 
 	// Run `terraform output` to get the values of output variables
-	actualTextExample := terraform.Output(t, terraformOptions, "ip_load_balancer")
-	actualExampleList1 := terraform.OutputList(t, terraformOptions, "ip_masters")
-	actualExampleList2 := terraform.OutputList(t, terraformOptions, "ip_workers")
+	ip_load_balancer := terraform.Output(t, terraformOptions, "ip_load_balancer")
+	ip_masters := terraform.OutputList(t, terraformOptions, "ip_masters")
+	ip_workers := terraform.OutputList(t, terraformOptions, "ip_workers")
 
 	// Verify we're getting back the outputs we expect
-	assert.Equal(t, expectedText, actualTextExample)
-	assert.Equal(t, expectedList, actualExampleList1)
-	assert.Equal(t, expectedList, actualExampleList2)
+	assert.Equal(t, exp_ip_load_balancer, ip_load_balancer)
+	assert.Equal(t, exp_ip_masters, ip_masters)
+	assert.Equal(t, exp_ip_workers, ip_workers)
+}
+
+// Deploy CaaSP using skuba
+func skuba(t *testing.T) {
+	test_structure.RunTestStage(t, "skubaInit", func() {
+		skubaInit(t)
+	})
+	test_structure.RunTestStage(t, "skubaBootstrap", func() {
+		skubaBootstrap(t)
+	})
+	test_structure.RunTestStage(t, "skubaJoin1", func() {
+		skubaJoin1(t)
+	})
+	test_structure.RunTestStage(t, "skubaJoin2", func() {
+		skubaJoin2(t)
+	})
+}
+
+func skubaInit(t *testing.T) {
+  cluster := "company-cluster"
+  expectedText := "[bootstrap] successfully bootstrapped core add-ons on node \"10.17.2.0\""
+	defer os.RemoveAll(cluster)
+  cmdArgs := []string{}
+  cmdArgs = append(cmdArgs, "cluster", "init", "--control-plane", "testing-lb.caasp.local", cluster)
+
+  cmd := shell.Command{
+    Command: "skuba",
+    Args:    cmdArgs,
+  }
+  out, err :=  shell.RunCommandAndGetOutputE(t, cmd)
+  fmt.Println(out)
+  fmt.Println(err)
+	assert.Equal(t, expectedText, out)
+
+  assert.DirExists(t, "company-cluster")
+}
+
+func skubaBootstrap(t *testing.T) {
+  cmdArgs := []string{}
+  cmdArgs = append(cmdArgs, "node", "bootstrap", "--user", "sles", "--sudo", "--target", "10.17.2.0", "my-master")
+
+  cmd := shell.Command{
+    Command: "skuba",
+    Args:    cmdArgs,
+    WorkingDir: "company-cluster",
+  }
+  out, err :=  shell.RunCommandAndGetOutputE(t, cmd)
+
+  fmt.Println(out)
+  fmt.Println(err)
+}
+
+func skubaJoin1(t *testing.T) {
+  cmdArgs := []string{}
+  cmdArgs = append(cmdArgs, "node", "join", "--role", "worker", "--user", "sles", "--sudo", "--target", "10.17.3.0", "worker0")
+
+  cmd := shell.Command{
+    Command: "skuba",
+    Args:    cmdArgs,
+    WorkingDir: "company-cluster",
+  }
+  out, err :=  shell.RunCommandAndGetOutputE(t, cmd)
+  fmt.Println(out)
+  fmt.Println(err)
+}
+
+func skubaJoin2(t *testing.T) {
+  cmdArgs := []string{}
+  cmdArgs = append(cmdArgs, "node", "join", "--role", "worker", "--user", "sles", "--sudo", "--target", "10.17.3.1", "worker1")
+
+  cmd := shell.Command{
+    Command: "skuba",
+    Args:    cmdArgs,
+    WorkingDir: "company-cluster",
+  }
+  out, err :=  shell.RunCommandAndGetOutputE(t, cmd)
+  fmt.Println(out)
+  fmt.Println(err)
 }
